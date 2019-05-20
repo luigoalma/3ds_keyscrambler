5d067bf6e8c648e44fb4c9a4d4ca1f5724d9c5de
luigoalma <luigoalma@hotmail.com>
2019-05-20 19:11:58 +0100

    Just a little detail

--- misc/arm32le/scrambler.h
+++ misc/arm32le/scrambler.h
@@ -1,3 +1,4 @@
 // for the arm 32-bit little endian assembly implementation
 // but also something to note, pointers to data **should** be aligned by 4
+// Returned key and taken keys are treated as 128bit big endian values in memory
 void KeyScrambler(void* NormalKey, const void* KeyX, const void* KeyY);
